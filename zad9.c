a98330b57aa6235b8ef7497aa01eb86dad881ee0
Jacek Gryckiewicz <mrwp450@gmail.com>
2023-10-29 11:44:06 +0100

    zad9

---
+++ zad9.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//todo
+
+int abosluteIfNeg(int a) {
+    if (a < 0) {
+        a = abs(a);
+    }
+    return a;
+
+}
+
+int calcArea() {
+
+}
+
+int getInput() {
+    int a, b, c ,d;
+
+    printf("First point: ");
+    printf("\tFirst value a:");
+    scanf("%d", a);
+    printf("\tSecond value b:");
+    scanf("%d", b);
+    printf("Second point: ");
+    printf("\tFirst value c: ");
+    scanf("%d", c);
+    printf("\tFirst value d: ");
+    scanf("%d", d);
+
+}
+
+int main() {
+
+
+
+}
