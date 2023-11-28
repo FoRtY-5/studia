8eddf3952421ea5e43b2fef116756231e8d56937
forty-5 <mrwp450@gmail.com>
2023-11-28 01:45:15 +0100

    better args function

--- main.c
+++ main.c
@@ -2,19 +2,23 @@
 #include <unistd.h>
 
 
+int input[];
+
+void getArgs(int argc, char* argv[]) {
+    for (int i = 1; i < argc; i++) {
+        char* ac = argv[i];
+        int n = atoi(ac);
+        input[i - 1] = n;
+    }
+}
+
 // use for command line arguments
 int main(int argc, char* argv[]){
+    getArgs(argc, argv);
 
-    int input[argc - 1];
 
-    for (int i = 1; i < argc; i++) {
-        char *ac = argv[i];    
-        int n = atoi(ac);
-        input[i - 1] = n;    
-    }
-
-    for (int i = 0; i < argc; i++)
-    {
+//  -- use for printing --
+    for (int i = 0; i < argc - 1; i++) {
         printf("%d arg: %d \n", i, input[i]);
     }
     
